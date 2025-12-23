55dfa4523eb969eb5fae217814f4b0c3a1636614
bublik <rmk14082007@gmail.com>
2025-12-23 04:11:56 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,9 +1,9 @@
 def area(a, b):
-    if a <= 0 or b <= 0:
-        raise ValueError("Rectangle sides must be positive")
+    if a < 0 or b < 0:
+        raise ValueError("Negative side")
     return a * b
 
 def perimeter(a, b):
-    if a <= 0 or b <= 0:
-        raise ValueError("Rectangle sides must be positive")
+    if a < 0 or b < 0:
+        raise ValueError("Negative side")
     return 2 * (a + b)
